8953275ba9505875ea6b0f7d0586e4a5a59a105d
Diosdado D. Campo <csicebu@gmail.com>
2016-02-16 11:18:16 +0800

    - created blog and comment migrations - laravel ships already user migrations that comes with user   authentication, registration and password reset, so only need to add   first_name and last_name for this application

--- database/migrations/2016_02_16_030519_create_blogs_table.php
+++ database/migrations/2016_02_16_030519_create_blogs_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateBlogsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,14 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('blogs', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('content');
+            $table->integer('user_id')->index();
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('blogs');
     }
 }
--- database/migrations/2016_02_16_030532_create_comments_table.php
+++ database/migrations/2016_02_16_030532_create_comments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,14 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->rememberToken();
+            $table->string('content');
+            $table->integer('blog_id')->index();
+            $table->integer('user_id')->index();
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('comments');
     }
 }
